974f7ef979b265dd9b41172ef501b33fe855d557
liwentao <liwentao@qq.com>
2019-09-09 14:35:02 +0800

    Initial commit

---
+++ src/main/java/com/example/file2021/File2021Application.java
@@ -0,0 +1,13 @@
+package com.example.file2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class File2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(File2021Application.class, args);
+    }
+
+}
